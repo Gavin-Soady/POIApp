a3aa77acd9186015e0e4df55817d0e28a8ac5326
Gavin Soady <60347182+Gavin-Soady@users.noreply.github.com>
2021-03-23 10:00:05 +0000

    added gallery routes

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@
 
 const Accounts = require("./app/controllers/accounts");
 const POI = require("./app/controllers/pois");
+const Gallery = require('./app/controllers/gallery');
 
 module.exports = [
   { method: "GET", path: "/", config: Accounts.index },
@@ -16,6 +17,17 @@ module.exports = [
   { method: "GET", path: "/home", config: POI.home },
   { method: "POST", path: "/addpoi", config: POI.add },
   { method: "GET", path: "/report", config: POI.report },
+
+  { method: 'GET', path: '/gallery/{_id}', config: Gallery.index },
+  { method: 'POST', path: '/uploadfile/{_id}', config: Gallery.uploadFile },
+  { method: 'GET', path: '/deleteimage/{public_id}', config: Gallery.deleteImage },
+  { method: 'GET', path: '/upload', config: Gallery.upload },
+
+  {method: "GET", path: "/delete-poi/{_id}", config: POI.deletepoi },
+  {method: 'GET', path: '/update-poi/{_id}', config: POI.showUpdatePOI},
+  {method: 'POST', path: "/update-poi/{_id}", config: POI.updatePOI},
+
+
 
   {
     method: "GET",
